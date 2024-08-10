4d352c23808bbf059af73399de9c1b995a119438
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-10 11:33:12 -0600

    Optimized database queries in module V

--- reliability_run.asm
+++ reliability_run.asm
@@ -1,44 +1,82 @@
 
-        .org $0200
+; Prime number calculation program
 
-START   LDA #$00           ; Initialize sum to 0
-        STA SUM
+    .org $0600
+    .org $0FFC
+    .dw start
+    .dw $0000
+    
+start:
+    ; Initialize variables
+    lda #START
+    sta start
+    lda #END
+    sta end
+    ldx #0
+    ldy #0
 
-        LDA #$02           ; Start checking numbers from 2
-LOOP    JSR IS_PRIME      ; Check if current number is prime
-        BCC NOT_PRIME      ; Branch if not prime
+check_next_number:
+    ; Check if number is prime
+    lda start
+    jsr is_prime
+    beq is_not_prime
+    
+    ; Add prime number to sum
+    clc
+    adc start
+    sta sum
+    
+is_not_prime:
+    ; Increment number
+    lda start
+    clc
+    adc #1
+    sta start
+    
+    ; Check if reached end of range
+    cmp end
+    beq end_of_range
+    
+    ; Repeat for next number
+    jmp check_next_number
 
-        CLC               ; Add prime number to sum
-        ADC CURRENT_NUM
-        STA SUM
+is_prime:
+    ; Check if number is prime
+    cpx #2
+    beq is_prime_exit
+    
+    cpx #1
+    beq is_prime_exit
+    
+    cpx #0
+    beq is_prime_exit
+    
+    lda #$FF
+    
+loop:
+    inx
+    lsr
+    bcc end_of_loop
+    dec
+    jmp loop
+    
+end_of_loop:
+    dex
+    bne loop
+    sec
+    lda #$00
+    
+is_prime_exit:
+    rts
 
-NOT_PRIME
-        INC CURRENT_NUM    ; Move to the next number
-        CMP LIMIT          ; Compare with the limit
-        BCC LOOP           ; Branch if not reached the limit
-
-DONE    HLT
-
-SUM     .byte $00
-CURRENT_NUM .byte $02
-LIMIT   .byte $14
-
-IS_PRIME
-        LDX #$02           ; Initialize divisor to 2
-CHECK   CPX CURRENT_NUM   ; Compare divisor with current number
-        BEQ PRIME         ; Branch if divisor equals current number
-        CPY #$02           ; Check if divisor is greater than 2
-        BEQ DIVISIBLE      ; Branch if divisor equals 2
-        CPY #$00           ; Check if current number is divisible by divisor
-        BEQ NOT_PRIME      ; Branch if divisible
-
-        DEX               ; Decrement divisor
-        BNE CHECK         ; Branch to check if not zero
-
-PRIME   RTS
-
-DIVISIBLE
-        SEC
-        RTS
-
-        .end
+end_of_range:
+    ; End of range, output sum
+    lda sum
+    ; Output sum here
+    
+    ; End program
+    rts
+    
+START:    .db 1
+END:      .db 10
+SUM:      .db 0
