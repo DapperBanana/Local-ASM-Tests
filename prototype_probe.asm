4bacf8f04593f241fd085a72d62c598d0231200e
Austin L. Howard <austin.lane.howard@gmail.com>
2024-08-17 12:11:33 -0600

    Integrated unit test coverage for critical paths

--- prototype_probe.asm
+++ prototype_probe.asm
@@ -1,38 +1,78 @@
 
-; Remove duplicates from a list while preserving order
-LDA #0         ; Initialize index i = 0
-STA index
+    .begin
 
-LDA #0         ; Initialize index j = 0
-STA j
+    LDX #0      ; X register for player choice
+    LDY #0      ; Y register for computer choice
 
-LDA list,X     ; Load initial value from list
-STA prevValue  ; Store it as prevValue
+  again:
+    LDA #$00    ; Prompt for user input
+    JSR $FFD2   ; Print the prompt
+    LDA #$01
+    JSR $FFD2   ; Print 'Rock (1)'
 
-loop:
-    LDA list,X         ; Load current value from list
-    CMP prevValue      ; Compare with prevValue
-    BEQ skipDuplicate  ; If current value is same as previous, skip
+    LDA #$00    ; Prompt for user input
+    JSR $FFD2   ; Print the prompt
+    LDA #$02
+    JSR $FFD2   ; Print 'Paper (2)'
 
-    STA output,Y       ; Store current value in output
-    INY                ; Increment output index Y
+    LDA #$00    ; Prompt for user input
+    JSR $FFD2   ; Print the prompt
+    LDA #$03
+    JSR $FFD2   ; Print 'Scissors (3)'
 
-skipDuplicate:
-    STA prevValue      ; Update prevValue
+    LDA #$00    ; Prompt for user input
+    JSR $FFD2   ; Print the prompt
+    LDA #$04
+    JSR $FFD2   ; Print 'Enter your choice (1-3): '
 
-    INX                ; Increment list index X
-    CMP #10            ; Check if end of list
-    BNE loop           ; If not end, go back to loop
+    LDA #$00    ; Get user input
+    JSR $FFD2   ; Print the prompt
+    LDA #$02
+    JSR $FFCB   ; Get user input
 
-    RTS                ; Return from subroutine
+    CMP #$31    ; Check if user input is valid
+    BCC again   ; Loop again if invalid
+    CMP #$34
+    BCS again
 
-list:
-    .BYTE 3, 5, 2, 5, 4, 6, 2, 1, 3, 4, 7, 8, 5, 9, 1, 2, 3, 4, 5, 6
-output:
-    .BYTE 20   ; Output array to store unique values
-index:
-    .BYTE 0    ; Index i
-j:
-    .BYTE 0    ; Index j
-prevValue:
-    .BYTE 0    ; Previous value
+    TAX         ; Store user choice in X register
+
+    LDA #$FF    ; Get random number for computer choice
+    JSR $FFEB   ; Get random number
+    AND #$03
+    INY         ; Store random number in Y register
+
+    ; Compare user choice with computer choice
+    CMP #1
+    BEQ rock
+    CMP #2
+    BEQ paper
+    CMP #3
+    BEQ scissors
+
+rock:
+    CPY #3      ; Computer chose scissors
+    BNE you_win
+    JMP draw
+
+paper:
+    CPY #1      ; Computer chose rock
+    BNE you_win
+    JMP draw
+
+scissors:
+    CPY #2      ; Computer chose paper
+    BNE you_win
+    JMP draw
+
+you_win:
+    LDA #$00
+    JSR $FFD2   ; Print 'You win!'
+
+draw:
+    LDA #$00
+    JSR $FFD2   ; Print 'It's a draw!'
+
+    JMP again   ; Play again
+
+    .end
