635458118399415ca5b37688e012010030e638e7
Austin L. Howard <austin.lane.howard@gmail.com>
2024-12-02 18:57:17 -0700

    Implemented asynchronous processing in feature X

--- preliminary_probe.asm
+++ preliminary_probe.asm
@@ -1,111 +1,32 @@
 
-        ORG $0200
+        .org $1000
+start   LDX #$05        ; Load X register with the number for which factorial is to be calculated
+        JSR factorial   ; Jump to factorial subroutine
+        BRK             ; Break
 
-start   LDA #12           ; Load first number into accumulator
-        STA num1          ; Store it in memory location num1
-        LDA #18           ; Load second number into accumulator
-        STA num2          ; Store it in memory location num2
-        
-        JSR find_lcm     ; Call subroutine to find LCM
-        
-end     BRK
+factorial
+        CPX #$00        ; Check if X register is 0
+        BEQ end         ; If X is 0, return 1
+        DEX             ; Decrement X register
+        JSR factorial   ; Recursive call to factorial subroutine
+        PHA             ; Push return value (result of recursive call) onto the stack
+        LDY #$01        ; Load Y register with 1
+        TXA             ; Transfer X register (current number) to A register
+loop    JSR multiply    ; Call multiply subroutine to multiply accumulator by Y
+        DEY             ; Decrement Y register
+        BNE loop        ; Continue looping until Y is 0
+        PLA             ; Pop the result of recursive call from stack
+        RTS             ; Return from subroutine
 
-find_lcm
-        LDA num1          ; Load first number into accumulator
-        STA temp1
-        LDA num2          ; Load second number into accumulator
-        STA temp2
-        
-        LDA #1            ; Initialize counter to 1
-        STA counter
-        
-loop    LDA temp1         ; Load temp1 into accumulator
-        CMP #$00          ; Compare with 0
-        BEQ end_loop1     ; If temp1 is 0, end loop
-        
-        LDY #0            ; Initialize index register Y to 0
-        
-loop1   LDX counter       ; Load counter into index register X
-        LDA temp1         ; Load temp1
-        
-        JSR divide        ; Call subroutine to divide temp1 by counter
-        
-        BNE check_loop1   ; If division was successful, check loop1
-        
-        INX               ; Increment counter
-        
-check_loop1
-        INY               ; Increment index register Y
-        CPY #$02          ; Compare index register Y with constant 2
-        BNE loop1         ; If not equal to 2, loop1
-        
-        JMP update_temp1  ; Jump to update_temp1
-        
-end_loop1
-        LDA #1            ; Initialize counter to 1
-        STA counter
-        
-loop2   LDA temp2         ; Load temp2 into accumulator
-        CMP #$00          ; Compare with 0
-        BEQ end_loop2     ; If temp2 is 0, end loop
-        
-        LDY #0            ; Initialize index register Y to 0
-        
-loop2   LDX counter       ; Load counter into index register X
-        LDA temp2         ; Load temp2
-        
-        JSR divide        ; Call subroutine to divide temp2 by counter
-        
-        BNE check_loop2   ; If division was successful, check loop2
-        
-        INX               ; Increment counter
-        
-check_loop2
-        INY               ; Increment index register Y
-        CPY #$02          ; Compare index register Y with constant 2
-        BNE loop2         ; If not equal to 2, loop2
-        
-        JMP update_temp2  ; Jump to update_temp2
+multiply
+        SEC             ; Set carry flag
+        ADC $0001       ; Add the value stored in memory location $0001 to accumulator
+        STA $0001       ; Store the result back in memory location $0001
+        SBC #$00        ; Subtract 0 from accumulator (discard carry)
+        RTS             ; Return from subroutine
 
-end_loop2
-        LDA temp1         ; Load temp1
-        STA result        ; Store the result in memory location result
+end     LDA #$01        ; Load accumulator with 1
+        RTS             ; Return from subroutine
 
-        LDX temp2         ; Load temp2
-        CLC               ; Clear carry flag
-        LDA result        ; Load result
-        ADC temp2         ; Add temp2 to result
-        STA result        ; Store the result in memory location result
-        
-        RTS               ; Return from subroutine
-
-divide  LDX #$00          ; Initialize index register X to 0
-
-loop_div
-        LDA #$00          ; Clear accumulator
-        CPX #$00          ; Compare index register X with constant 0
-        BNE skip_div      ; If not equal to 0, skip the division
-
-        INC X             ; Increment index register X
-
-skip_div
-        CLC               ; Clear carry flag
-        LDA temp1         ; Load temp1
-        SBC temp2         ; Subtract temp2
-        STA temp1         ; Store the result back in temp1
-
-        CPX #$00          ; Compare index register X with constant 0
-        BNE loop_div      ; If not equal to 0, loop_div
-
-        SEC               ; Set carry flag
-        
-        RTS               ; Return from subroutine
-
-num1    .BYTE $00       ; First number
-num2    .BYTE $00       ; Second number
-temp1   .BYTE $00       ; Temporary variable 1
-temp2   .BYTE $00       ; Temporary variable 2
-counter .BYTE $00       ; Counter variable
-result  .BYTE $00       ; Result variable
-
-        END start
+        .org $FFFA
+        .word start
