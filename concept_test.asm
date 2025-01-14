8c5c6e79903a1941a9bd017e215924ac3ff93659
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-14 12:14:44 -0700

    Integrated feedback from usability testing

--- concept_test.asm
+++ concept_test.asm
@@ -1,66 +1,78 @@
 
-; Factorial Calculation Using Recursion
-; Input: Number stored in memory location 'number'
-; Output: Factorial of the input number stored in memory location 'result'
+ORG $1000
 
-    .text
+; Constants
+LIST1_START = $1000
+LIST2_START = $2000
+MERGED_LIST_START = $3000
+MAX_SIZE = 10
+
+; Variables
+list1_ptr = $00
+list2_ptr = $01
+merged_ptr = $02
+temp_ptr = $03
+temp_value = $04
+i = $05
+j = $06
+
+; Initialize pointers
+    LDA #LIST1_START
+    STA list1_ptr
+    LDA #LIST2_START
+    STA list2_ptr
+    LDA #MERGED_LIST_START
+    STA merged_ptr
+
+merge_lists_loop:
+    ; Compare elements at list1_ptr and list2_ptr
+    LDA (list1_ptr),Y
+    CMP (list2_ptr),Y
+    BCC add_list1_element
+    ; If list2 element is smaller, add it to merged list
+    LDA (list2_ptr),Y
+    STA (merged_ptr),Y
+    JMP increment_list2_ptr
     
-    .org $0200
-    
-    ; Entry point of the program
-    start:
-        LDA #6          ; Load the input number (change as needed)
-        STA number      ; Store the input number in 'number' memory location
-        
-        JSR factorial   ; Call the factorial function
-        
-        LDA result      ; Load the result
-        ; Display the result here (store in memory location, print to screen, etc.)
-        
-        JMP $FFFC       ; End of program
-    
-    ; Factorial function
-    factorial:
-        JSR push        ; Save current state on stack
-        
-        LDA number      ; Load number
-        BNE continue    ; If number is not zero, continue with recursion
-        
-        LDA #1          ; If number is zero, return 1 as factorial
-        JMP done        ; End recursive call
-        
-    continue:
-        DEC number      ; Decrement number
-        JSR factorial   ; Recursive call
-        
-        TAX             ; Save return value on stack
-        JSR pop         ; Restore previous state from stack
-        
-        ; Calculate factorial and store in 'result'
-        LDA number
-        CLC
-        ADC result
-        STA result
-        
-    done:
-        RTS
-    
-    ; Stack operations
-    push:
-        PHA
-        TXA
-        PHA
-        RTS
-        
-    pop:
-        PLA
-        TAX
-        PLA
-        RTS
-    
-    ; Data storage
-    .org $0260
-    number: .byte 0     ; Input number
-    result: .byte 0     ; Factorial result
-    
-    .org $02FF
+add_list1_element:
+    ; Add list1 element to merged list
+    LDA (list1_ptr),Y
+    STA (merged_ptr),Y
+    JMP increment_list1_ptr
+
+increment_list1_ptr:
+    ; Increment list1 pointer
+    CLC
+    ADC #1
+    STA temp_value
+    LDA temp_value
+    CMP #MAX_SIZE
+    BEQ end_merge
+    LDA list1_ptr
+    STA temp_ptr
+    LDX #0
+    INX
+    STX temp_ptr
+    LDA temp_value
+    STA list1_ptr
+    JMP merge_lists_loop
+
+increment_list2_ptr:
+    ; Increment list2 pointer
+    CLC
+    ADC #1
+    STA temp_value
+    LDA temp_value
+    CMP #MAX_SIZE
+    BEQ end_merge
+    LDA list2_ptr
+    STA temp_ptr
+    LDX #0
+    INX
+    STX temp_ptr
+    LDA temp_value
+    STA list2_ptr
+    JMP merge_lists_loop
+
+end_merge:
+    RTS
