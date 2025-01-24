7e5aa3d23e440bc94183099fd901623a678f4113
Austin L. Howard <austin.lane.howard@gmail.com>
2025-01-23 19:06:25 -0700

    Enhanced user authentication for improved security

--- mock_test.asm
+++ mock_test.asm
@@ -1,108 +1,88 @@
 
-; 6502 Assembly Calculator Program
+* Program to read and analyze data from a CSV file
 
-    .org $0200   ; Start address
+* Constants
+FILE_NAME = "data.csv"  ; Name of the CSV file
+BUFFER_SIZE = 256       ; Size of buffer for reading data
+NUM_VALUES = 10         ; Number of values in the CSV file
 
-    LDA #$00     ; Initialize accumulator to 0
-    STA result
+* Variables
+buffer        .ds BUFFER_SIZE ; Buffer for reading data from file
+values        .ds NUM_VALUES  ; Array to store values from CSV file
+index         .byte 0        ; Index into values array
+sum           .word 0        ; Sum of values
+average       .word 0        ; Average of values
 
-loop:
-    ; Display menu options
-    LDA #<menu
-    LDY #>menu
-    JSR displayText
+* Entry point
+          .org $1000
+          lda #FILE_NAME
+          jsr open_file         ; Open the CSV file for reading
+          jsr read_values       ; Read values from the file
+          jsr calculate_average ; Calculate the average of the values
+          jsr close_file        ; Close the file
+          lda average           ; Load average into A register
+          sta $D020             ; Store average in $D020 for display
+          rts
 
-    ; Get user input
-    JSR getInput
+* Subroutine to open a file
+open_file lda #1               ; Open file for reading
+          sta $9A               ; Set X register to 1
+          lda #3                ; Mode for opening file
+          jsr $FFD5             ; Call CIO
+          rts
 
-    ; Perform operation based on user input
-    CMP #$01
-    BEQ add
-    CMP #$02
-    BEQ sub
-    CMP #$03
-    BEQ mul
-    CMP #$04
-    BEQ div
-    CMP #$05
-    BEQ exit
+* Subroutine to read values from file
+read_values lda #BUFFER_SIZE    ; Buffer length
+           ldx #buffer          ; Buffer address
+           jsr $FFBD            ; Read data from file
+           ldx #buffer          ; Load buffer address into X register
+           ldy #0               ; Initialize Y register to 0
+next_char  lda (x),y            ; Load next character from buffer
+           beq end_of_data      ; Exit loop if end of buffer
+           cmp #','             ; Compare character to comma
+           beq end_of_value     ; If comma, end of value
+           sec                   ; Set carry flag
+           sbc #48              ; Subtract ASCII '0'
+           sta values,x         ; Store numeric value
+           dex                  ; Decrease X register
+           bpl next_char        ; Continue loop
+end_of_value inx
+           ldy #0               ; Reset Y register
+           jmp next_char        ; Continue loop
+end_of_data rts
 
-    ; Invalid option
-    JMP loop
+* Subroutine to calculate the average value
+calculate_average ldx #NUM_VALUES ; Number of values to process
+                 lda #0           ; Initialize sum to 0
+                 txa
+loop             lda values,x        ; Load value from array
+                 clc
+                 adc sum            ; Add value to sum
+                 sta sum            ; Store sum
+                 dex
+                 bne loop           ; Repeat loop for all values
+                 ldx #NUM_VALUES
+                 lda sum             ; Load sum
+                 adc #0               ; Add the carry
+                 jsr divide          ; Divide sum by number of values
+                 lda quotient        ; Load quotient into A register
+                 sta average         ; Store average
+                 rts
 
-add:
-    JSR getOperands
-    CLC
-    ADC num1
-    STA result
-    JMP loop
+* Subroutine to close a file
+close_file lda #7               ; Close the file
+           jsr $FFD5            ; Call CIO
+           rts
 
-sub:
-    JSR getOperands
-    SEC
-    SBC num1
-    STA result
-    JMP loop
+* Subroutine to divide
+divide     clc
+           lda sum              ; Load sum
+           ldx #NUM_VALUES      ; Load num of values
+           div         ; Divide sum by NUM_VALUES
+           sta quotient
+           rts
 
-mul:
-    JSR getOperands
-    LDA num1
-    CLC
-    ADC num1
-    STA result
-    JMP loop
+* File data.csv with values separated by commas
+.data
+          .byte 1,2,3,4,5,6,7,8,9,10
 
-div:
-    JSR getOperands
-    LDA num1
-    SEC
-    SBC num1
-    STA result
-    JMP loop
-
-exit:
-    RTS
-
-displayText:
-    LDX #$00
-loop1:
-    LDA (X),Y
-    BEQ done
-    JSR $FFD2
-    INX
-    JMP loop1
-done:
-    RTS
-
-getInput:
-    JSR $FFCF
-    RTS
-
-getOperands:
-    LDX #$00
-    LDA #$00
-loop2:
-    JSR getInput
-    STA operand,X
-    INX
-    CPX #$02
-    BNE loop2
-    RTS
-
-menu:
-    .ascii "Calculator\0"
-    .byte "$01. Add\0"
-    .byte "$02. Subtract\0"
-    .byte "$03. Multiply\0"
-    .byte "$04. Divide\0"
-    .byte "$05. Exit\0"
-
-result:
-    .byte $00
-num1:
-    .byte $00
-operand:
-    .byte $00
-    .byte $00
-
-    .end
