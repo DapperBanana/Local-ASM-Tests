b95a2cf98e3e48e01f3cc416e5aa60a0ffce174d
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-05 17:52:40 -0700

    Enhanced error logging for production monitoring

--- initial_run.asm
+++ initial_run.asm
@@ -1,24 +1,97 @@
 
-    .org $0200
-    
-    ldx #0        ; Initialize index register X to 0
-    lda #0        ; Initialize accumulator to 0
-loop:
-    lda data,x   ; Load value at index X into accumulator
-    clc          ; Clear carry flag
-    adc acc       ; Add value to accumulator
-    sta acc       ; Store result back in accumulator
-    inx          ; Increment index
-    cpx #length  ; Compare index to length of the list
-    bne loop     ; If not at the end of the list, repeat loop
-    
-    ; At this point, the sum of all elements in the list is stored in the accumulator
-    
-    ; Your code here to use or display the sum
-    
-data:
-    .byte $01, $02, $03, $04, $05   ; List of elements to sum
-length:
-    .byte 5                        ; Length of the list
+        .org $0200
 
-    .end
+init:
+        lda #$ff                ; Initialize random number generator
+        sta RAND_SEED
+
+generate_maze:
+        ldx #0                  ; Initialize loop counter
+create_maze_row:
+        lda #$00                ; Clear current row
+        sta MAZE_ROW,X
+        inx
+        cpx MAZE_WIDTH
+        bne create_maze_row
+
+        ldy #0                  ; Initialize loop counter
+create_maze_col:
+        lda RAND_SEED           ; Get random value
+        clc
+        adc #$01                ; Increment value
+        and #$0f                ; Keep only lower 4 bits
+        sta RAND_SEED
+
+        sta TEMP1
+        ldx MAZE_WIDTH          ; Set loop counter to maze width
+generate_wall:
+        lda TEMP1               ; Set wall at random position in row
+        and WALL_BIT_MASK
+        ora MAZE_ROW,Y
+        sta MAZE_ROW,Y
+
+        dey
+        bpl generate_wall
+
+        iny
+        cpy MAZE_HEIGHT
+        bne create_maze_col
+
+        rts
+
+solve_maze:
+        ldx #0                  ; Set starting position
+        ldy #0
+
+find_solution:
+        lda MAZE_ROW,X          ; Check if maze is solved
+        cmp #$ff
+        beq maze_solved
+
+        check_up:
+        lda MAZE_ROW,X          ; Check if there is a path above
+        and #$01
+        cmp #$01
+        bne check_down
+
+        dey                     ; Move up
+        bpl find_solution
+
+        check_down:
+        lda MAZE_ROW,X          ; Check if there is a path below
+        and #$08
+        cmp #$08
+        bne check_right
+
+        iny                     ; Move down
+        bne find_solution
+
+        check_right:
+        lda MAZE_ROW,X          ; Check if there is a path to the right
+        and #$02
+        cmp #$02
+        bne check_left
+
+        inx                     ; Move right
+        bne find_solution
+
+        check_left:
+        lda MAZE_ROW,X          ; Check if there is a path to the left
+        and #$04
+        cmp #$04
+        beq maze_solved
+
+        dex                     ; Move left
+        bne find_solution
+
+maze_solved:
+        rts
+
+RAND_SEED:      .byte 0
+WALL_BIT_MASK:  .byte $0f
+MAZE_WIDTH:     .byte 10
+MAZE_HEIGHT:    .byte 10
+MAZE_ROW:       .byte 100 dup (0)
+TEMP1:          .byte 0
+
+        .end
