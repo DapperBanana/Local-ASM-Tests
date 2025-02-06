b1cec169a277844d5b8dbba9bee040d93c264cea
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-05 17:48:16 -0700

    Reverted changes in file Q for compatibility

--- trial_script.asm
+++ trial_script.asm
@@ -1,157 +1,39 @@
 
-.segment "CHAR"
-message_start
-.byte "Welcome to the Personality Quiz! Answer a few questions to find out more about yourself.", 0
-message_end
+        .org $1000
 
-.segment "DATA"
-question1
-.byte "1. Are you more introverted or extroverted? (I/E)", 0
-question2
-.byte "2. Do you prefer routine or spontaneity? (R/S)", 0
-question3
-.byte "3. Are you more logical or emotional? (L/E)", 0
-question4
-.byte "4. Would you rather work alone or in a team? (A/T)", 0
-question5
-.byte "5. Are you a planner or do you like to go with the flow? (P/G)", 0
+checkSquare:
+        lda $00  ; Load the given number into the accumulator
+        cmp #$00 ; Compare it with 0
+        beq isSquare ; If it is 0, then it is a perfect square
+        
+        ldx $00  ; Load the given number into the X register
+        lda #$01 ; Load 1 into the accumulator
+        sta $01  ; Store 1 in memory location $01
+        clc      ; Clear the carry flag
+        
+innerLoop:
+        lda $01   ; Load the current number being checked into the accumulator
+        adc $01   ; Add the current number to itself
+        cmp $00   ; Compare the result with the given number
+        bcc nextNum ; If less than the given number, go to next number
+        
+        beq isSquare ; If equal to the given number, then it is a perfect square
+        
+        jmp notSquare ; If greater than the given number, then it is not a perfect square
+        
+nextNum:
+        inx     ; Increment the current number
+        cpx $00 ; Compare the current number with the given number
+        bne innerLoop ; If not equal, repeat the inner loop
+        
+        jmp notSquare ; If no perfect square found, then it is not a perfect square
 
-answers
-.byte 0,0,0,0,0
+isSquare:
+        lda #$01 ; Load 1 into the accumulator (signifying it is a perfect square)
+        rts      ; Return from subroutine
+        
+notSquare:
+        lda #$00 ; Load 0 into the accumulator (signifying it is not a perfect square)
+        rts      ; Return from subroutine
 
-.segment "BSS"
-response
-.byte 0
-
-.segment "CODE"
-.org $8000
-
-start:
-    lda #message_start
-    jsr print_string
-    
-    lda #question1
-    jsr print_string
-    jsr get_response
-    sta answers
-    
-    lda #question2
-    jsr print_string
-    jsr get_response
-    sta answers+1
-    
-    lda #question3
-    jsr print_string
-    jsr get_response
-    sta answers+2
-    
-    lda #question4
-    jsr print_string
-    jsr get_response
-    sta answers+3
-    
-    lda #question5
-    jsr print_string
-    jsr get_response
-    sta answers+4
-    
-    lda #message_end
-    jsr print_string
-    
-    lda answers
-    jsr print_answer
-    
-done:
-    rts
-
-print_string:
-    ldx #0
-print_loop:
-    lda [x]
-    beq print_done
-    jsr $ffd2 ; CHROUT
-    inx
-    jmp print_loop
-print_done:
-    rts
-
-print_answer:
-    lda response
-    sta $d020 ; clear screen
-    cmp #"I"
-    beq introverted
-    cmp #"E"
-    beq extroverted
-    cmp #"R"
-    beq routine
-    cmp #"S"
-    beq spontaneity
-    cmp #"L"
-    beq logical
-    cmp #"E"
-    beq emotional
-    cmp #"A"
-    beq alone
-    cmp #"T"
-    beq team
-    cmp #"P"
-    beq planner
-    cmp #"G"
-    beq flow
-    
-    rts
-    
-introverted:
-    lda #"You are more introverted and prefer spending time alone."
-    jsr print_string
-    rts
-
-extroverted:
-    lda #"You are more extroverted and enjoy socializing with others."
-    jsr print_string
-    rts
-
-routine:
-    lda #"You prefer routine and structure in your daily life."
-    jsr print_string
-    rts
-
-spontaneity:
-    lda #"You enjoy spontaneity and are always up for new adventures."
-    jsr print_string
-    rts
-
-logical:
-    lda #"You tend to approach situations with logic and reason."
-    jsr print_string
-    rts
-
-emotional:
-    lda #"You make decisions based on your emotions and feelings."
-    jsr print_string
-    rts
-
-alone:
-    lda #"You work best independently and value your personal space."
-    jsr print_string
-    rts
-
-team:
-    lda #"You thrive in collaborative environments and enjoy working with others."
-    jsr print_string
-    rts
-
-planner:
-    lda #"You like to plan ahead and have a clear path for the future."
-    jsr print_string
-    rts
-
-flow:
-    lda #"You prefer to go with the flow and adapt to changing circumstances."
-    jsr print_string
-    rts
-
-get_response:
-    lda #response
-    jsr $ffb5 ; GETIN
-    rts
+        .end
