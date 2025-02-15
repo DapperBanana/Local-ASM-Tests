fd4f1c66f49a7d6eb86b1c8e0f9886a719cfd637
Austin L. Howard <austin.lane.howard@gmail.com>
2025-02-15 15:20:25 -0700

    Fixed edge case scenario in module U

--- reliability_check.asm
+++ reliability_check.asm
@@ -1,79 +1,127 @@
 
-; Generate a random walk plot using Matplotlib
+; Program to calculate the area of a regular polygon
+; Formula for area of regular polygon: area = (n * s^2) / (4 * tan(PI / n))
+
+; Define constants
+PI equ 3.141592653589
+n equ 6 ; Number of sides in the regular polygon
+s equ 5 ; Length of each side in the regular polygon
 
 ; Define variables
-x_position = $00
-y_position = $01
-random_num = $02
+area equ $ ; Store the result in the area variable
 
-; Initialize seed for random number generation
-jsr srand
-
-; Set initial position
-lda #$50   ; x position
-sta x_position
-lda #$50   ; y position
-sta y_position
-
-; Loop to generate random walk data
-ldx #100   ; Number of steps
-loop:
-; Generate random number between -1 and 1
-jsr random
-sta random_num
-cmp #$80
-bmi minus   ; If random number is negative, subtract 1 from x position
-plus:
-clc
-adc x_position
-sta x_position
-jmp end_loop
-
-minus:
-sec
-sbc x_position
-sta x_position
-
-end_loop:
-; Generate random number between -1 and 1
-jsr random
-sta random_num
-cmp #$80
-bmi minus_y   ; If random number is negative, subtract 1 from y position
-plus_y:
-clc
-adc y_position
-sta y_position
-jmp next_step
-
-minus_y:
-sec
-sbc y_position
-sta y_position
-
-next_step:
-dex
-bne loop
-
-; Plot the random walk using Matplotlib
-lda x_position
-sta $0200  ; Set x coordinate in memory location $0200
-lda y_position
-sta $0201  ; Set y coordinate in memory location $0201
+; Initialization
+    lda #n
+    jsr get_tan
+    sta $f0 ; tan(PI / n)
+    lda #s
+    clc
+    adc #$00
+    adc #$00
+    sta $f1 ; s^2
+    lda #10
+    sta $f2 ; Multiplication factor (10 = 4 * 2.5)
+    ldx $f1 ; s^2
+    ldy $f0 ; tan(PI / n)
+    jsr multiply
+    ldx $f2 ; (4 * tan(PI / n))
+    jsr div_16bit
+    sta area
 
 ; End of program
-rts
+    rts
 
-; Subroutines
+; Subroutine to calculate tan(PI / n)
+; Input: A = n
+; Output: A = tan(PI / n)
+get_tan:
+    asl
+    cmp #$05
+    beq .done
+    lsr
+    cmp #$02
+    bcs .calc_tan
+    lda #0
+    rts
 
-srand:
-ldx #$10  ; Seed value
-srng
-rts
+.calc_tan:
+    jsr power
+    ldy #5
+    ldx #2
+    jsr multiply
+    ldy #100
+    jsr div_16bit
+    rts
 
-random:
-lda $0202  ; Get seed value
-asl
-adc #$01
-sta $0202  ; Save new seed value
-rts
+.done:
+    lda #57408 ; Pre-calculated value of tan(PI / 6) * 256
+    rts
+
+; Subroutine to calculate power
+; Input: A = base, Y = exponent
+; Output: A = result
+power:
+    cpy #0
+    beq .done_power
+    sec
+    lda #1
+    sta $f1
+.loop_power:
+    jsr multiply
+    dey
+    bne .loop_power
+.done_power:
+    lda $f1
+    rts
+
+; Subroutine to multiply two 8-bit numbers
+; Input: X = first number, Y = second number
+; Output: Accumulator = result
+multiply:
+    sta $f2
+    ldy #0
+.loop_multiply:
+    clc
+    lda $f2
+    adc $f1
+    bcc .skip_inc
+    iny
+.skip_inc:
+    lsr
+    dey
+    bne .loop_multiply
+    rts
+
+; Subroutine to divide a 16-bit number by a 8-bit number
+; Input: 16-bit number in X and Accumulator, 8-bit number in Y
+; Output: Quotient in Accumulator
+div_16bit:
+    sta $f2
+    ldy #0
+    sty $f3
+.loop_div:
+    cmp $f2
+    bcc .done_div
+    lda $f2
+    sec
+    sbc $f3
+    clc
+    lda $f0
+    adc $f1
+    ror
+    sta $f2
+    ldy $f3
+    ror
+    sta $f3
+    jmp .loop_div
+.done_div:
+    lda $f3
+    rts
+
+; Data
+    .block 2
+$f0: .byte 0
+$f1: .byte 0
+$f2: .byte 0
+$f3: .byte 0
+area: .word 0
