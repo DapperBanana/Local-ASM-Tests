8f6e07ad5b7f13494f01ead8eb821f138ae55d52
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-10 18:34:22 -0600

    Fixed edge case scenario in module U

--- safety_test.asm
+++ safety_test.asm
@@ -1,53 +1,47 @@
 
-; File Compression Algorithm in 6502 Assembly
+START   LDX #0         ; Initialize index register X to 0
+        LDA MESSAGE,X ; Load the English message into the accumulator
+        BEQ DONE       ; If end of message, exit program
+        JSR TRANSLATE  ; Translate the English word into Spanish
+        STA $D000,X    ; Store the translated word in memory
+        INX            ; Increment index register
+        JMP START      ; Continue to next word
 
-; Load source address and destination address
-LDX #source_address
-LDY #dest_address
-LDA #0
+TRANSLATE CMP #$20      ; Compare the English word to space character
+        BEQ DONE        ; If space character, return without translation
+        CMP #$48        ; Compare the English word to "H"
+        BEQ HELLO       ; If "H", translate to "Hola"
+        CMP #$57        ; Compare the English word to "W"
+        BEQ WORLD       ; If "W", translate to "Mundo"
+        JMP DONE        ; If no match, return without translation
 
-compress_loop:
-; Load byte from source address
-LDA (X), y
+HELLO   LDA #$H4        ; Load "H" character in Spanish
+        STA ($D000),X  ; Store translated character in memory
+        INX             ; Increment index register
+        LDA #$6F        ; Load "o" character in Spanish
+        STA ($D000),X   ; Store translated character in memory
+        INX             ; Increment index register
+        LDA #$6C        ; Load "l" character in Spanish
+        STA ($D000),X   ; Store translated character in memory
+        INX             ; Increment index register
+        LDA #$61        ; Load "a" character in Spanish
+        STA ($D000),X   ; Store translated character in memory
+        RTS             ; Return from subroutine
 
-; Check if byte is repeated
-CMP (X), y+1
-BNE store_byte
+WORLD   LDA #$4D        ; Load "M" character in Spanish
+        STA ($D000),X  ; Store translated character in memory
+        INX             ; Increment index register
+        LDA #$75        ; Load "u" character in Spanish
+        STA ($D000),X   ; Store translated character in memory
+        INX             ; Increment index register
+        LDA #$6E        ; Load "n" character in Spanish
+        STA ($D000),X   ; Store translated character in memory
+        INX             ; Increment index register
+        LDA #$64        ; Load "d" character in Spanish
+        STA ($D000),X   ; Store translated character in memory
+        INX             ; Increment index register
+        RTS             ; Return from subroutine
 
-; If byte is repeated, count repetitions
-LDX #1
-repeated_loop:
-CPY #255
-BEQ store_byte
+DONE    BRK             ; Exit program
 
-; Count repetitions
-INC X
-INC Y
-CMP (X), y
-BEQ repeated_loop
-
-; Store the repeated byte and repetition count
-store_byte:
-STA (Y),X
-INY
-
-; Check if end of file is reached
-CPX #end_of_file
-BEQ end
-
-JMP compress_loop
-
-end:
-RTS
-
-; Data section
-source_address:
-.byte $00, $01, $01, $02, $02, $02
-.byte $03, $03, $03, $03, $04, $04
-
-dest_address:
-.byte $00
-.byte $01
-
-end_of_file:
-.byte $04
+MESSAGE .BYTE "HELLO WORLD", $00 ; English message to be translated
