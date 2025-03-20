a92076f9d468f50f112f564a20fb9c9b262863be
Austin L. Howard <austin.lane.howard@gmail.com>
2025-03-20 13:19:11 -0600

    Fixed broken links in the application

--- script_test.asm
+++ script_test.asm
@@ -1,35 +1,37 @@
 
-        .org $0200
-START   LDX #0
-        LDY #0
-GETCHAR LDA MESSAGE,Y
-        BEQ DONE
-        CMP #" "
-        BEQ REVERSE
-        INY
-        BNE GETCHAR
-REVERSE LDA #0
-        STA TEMP
-        DEY
-REVLOOP LDY TEMP
-        CMP MESSAGE,Y
-        BNE REVLOOP2
-        LDA MESSAGE,Y
-        STA MESSAGE,X
-        INX
-        LDA #0
-        STA MESSAGE,Y
-REVLOOP2 LDX TEMP
-        INX
-        TXA
-        CPY X
-        BEQ GETCHAR
-        LDX X
-        STX TEMP
-        JMP REVLOOP
-DONE    BRK
+START LDX #0         ; Initialize index for first string
+LOOP1 LDA STR1,X     ; Load character from first string
+     BEQ END1        ; If character is null, end of string reached
+     JSR CHECK_CHAR  ; Call subroutine to check character
+     INX             ; Increment index
+     JMP LOOP1       ; Continue to next character
 
-MESSAGE .byte "Hello World!", 0
-TEMP    .byte 0
+END1 LDX #0         ; Initialize index for second string
+LOOP2 LDA STR2,X     ; Load character from second string
+     BEQ END         ; If character is null, end of string reached
+     JSR CHECK_CHAR  ; Call subroutine to check character
+     INX             ; Increment index
+     JMP LOOP2       ; Continue to next character
+     
+END BRK             ; End of program
 
-        .end
+CHECK_CHAR
+     ; Check if character is in common between both strings
+     STX INDEX        ; Store index in INDEX variable
+     LDX #0           ; Initialize index for first string
+CHECK_LOOP
+     LDA COMMON,X     ; Load character from common characters
+     BEQ ADD_CHAR     ; If character is null, add current character
+     CMP STR1,X       ; Compare character with character from first string
+     BEQ ADD_CHAR     ; If characters match, add current character
+     INX              ; Increment index
+     JMP CHECK_LOOP   ; Check next character
+ADD_CHAR
+     STA COMMON,X     ; Add current character to common characters
+     STX INDEX        ; Store updated index
+     RTS
+
+INDEX .BYTE 0
+COMMON .BYTE 0
+STR1 .ASCII "hello"
+STR2 .ASCII "world"
