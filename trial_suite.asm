3a6b048ff349f85fa95754f5b16bf4bd0277546f
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-10 14:42:26 -0600

    Optimized SQL queries for faster execution

--- trial_suite.asm
+++ trial_suite.asm
@@ -1,117 +1,27 @@
 
         .org $0200
-
-        LDX #7          ; Load the number of sides (heptagon)
-        LDA #100        ; Load the length of each side (example: 100 units)
-        STA side_length ; Store the side length in memory
-        JSR calculate_area ; Calculate the area of the heptagon
-        BRK 
-
-calculate_area:
-        LDA side_length   ; Load the side length
-        CLC 
-        ADC side_length   ; Add side_length to A
-        STA perimeter     ; Store the perimeter in memory
+start   LDX #0
+        LDY #0
         
-        LDA perimeter     ; Load the perimeter
-        LSR
-        STA half_perimeter ; Store half of the perimeter
+main    LDA $0200,Y   ; print spaceship
+        CMP #0
+        BEQ game_over
+        JSR $ffd2      ; print character
+        INY
+        JMP main
         
-        LDX #0           ; Initialize the index for the loop
-        LDA #0           ; Initialize the sum of areas
-loop:
-        LDA half_perimeter ; Load half of the perimeter
-        SEC
-        SBC X             ; Subtract the index from half of the perimeter
-        STA d             ; Store the result in d
+game_over
+        LDX #0
+print   LDA message,X
+        CMP #0
+        BEQ end
+        JSR $ffd2
+        INX
+        JMP print
+
+end     BRK
+
+message .byte "Game Over!",0
         
-        LDA d            ; Load d
-        STA c            ; Store d in c
-        
-        ADC X            ; Add the index to d (c + index)
-        SEC 
-        SBC #1           ; Subtract 1 from c + index
-        STA d            ; Store the result in d
-        
-        LDA half_perimeter   ; Load half of the perimeter
-        SEC 
-        SBC d            ; Subtract d from half of the perimeter
-        STA a            ; Store the result in a
-        
-        LDA c             ; Load c
-        STA base_length   ; Store c as the base length
-        
-        LDA a             ; Load a
-        CLC
-        ADC side_length   ; Add the side length to a
-        STA side          ; Store the result in side
-
-        LDA base_length   ; Load the base length
-        LDA side          ; Load the side length
-
-        JSR calculate_triangle_area ; Calculate the area of each triangle
-
-        INX              ; Increment the index
-        CPX #7           ; Check if all sides have been processed
-        BNE loop         ; If not, loop again
-
-        RTS
-
-calculate_triangle_area:
-        LDA base_length   ; Load the base length
-        LDY side          ; Load the side length
-        CLC 
-        ADC Y            ; Add side length to the base length
-        STA L            ; Store the result in L
-
-        LDA base_length  ; Load the base length
-        LDY Y           ; Load the side length
-        CLC
-        ADC y           ; Add the side length to the base length
-        STA x           ; Store the result in x
-
-        LDA L            ; Load the L
-        LDY X            ; Load the index
-        CLC
-        ADC Y            ; Add the index to L
-        STA h            ; Store the result in h
-
-        LDA L            ; Load L
-        LDY x            ; Load x
-        CLC
-        ADC Y            ; Add x to L
-        STA h            ; Store the result in h
-
-        LDA h            ; Load h
-        LSR      
-        STA h            ; Divide h by 2
-        LSR
-        STA h
-
-        LDA h            ; Load h
-        LSR      
-        STA h            ; Divide h by 2
-
-        LDA h            ; Load h
-        LDY base_length  ; Load the base length
-        CLC
-        ADC Y            ; Add the base length to h
-        STA area         ; Store the result in area
-
-        RTS
-
-side_length .byte 0
-perimeter .byte 0
-half_perimeter .byte 0
-d .byte 0
-c .byte 0
-a .byte 0
-base_length .byte 0
-side .byte 0
-L .byte 0
-X .byte 0
-y .byte 0
-h .byte 0
-area .byte 0
-
-        .end
+        .org $fffc
+        .word $0200
