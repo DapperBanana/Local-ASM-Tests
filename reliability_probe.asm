1ae9332c0f66f91b9a2ced7a1a4953d9915eb587
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-15 18:34:11 -0600

    Refactored code for better scalability

--- reliability_probe.asm
+++ reliability_probe.asm
@@ -1,65 +1,93 @@
 
-    .text
-    .org $2000
+        .org    $0200   ; Start address of program
 
-checkSymmetric:
-    lda matrixSize
-    sta numRows
-    
-    lda numRows
-    clc
-    adc numRows
-    tax
-    lda #0
-    sta result
-    
-    lda numRows
-    
-checkRow:
-    lda #0
-    sta index1
-    
-    lda #0
-    sta index2
-    
-checkColumn:
-    lda index1
-    clc
-    adc index2
-    sta address1
-    
-    lda index2
-    clc
-    adc index1
-    sta address2
-    
-    lda (address1),y
-    cmp (address2),y
-    bne notSymmetric
-    
-    iny
-    lda numRows
-    cmp index2
-    bne checkColumn
-    
-    lda index1
-    clc
-    adc #1
-    sta index1
-    
-    lda numRows
-    cmp index1
-    bne checkRow
-    
-    lda numRows
-    clc
-    adc numRows
-    cmp index1
-    bne done
-    
-notSymmetric:
-    lda #1
-    sta result
-    
-done:
-    rts
+START   LDX     #RADIUS ; Load radius value into X register
+        LDY     #HEIGHT ; Load height value into Y register
+
+        JSR     CALC_VOLUME ; Call subroutine to calculate volume
+
+        ; Output the result (volume) to memory location $0300
+        STX     $0300   ; Store low byte of volume
+        STY     $0301   ; Store high byte of volume
+
+END     BRK            ; End of program
+
+CALC_VOLUME
+        ; Formula for calculating the volume of a cylinder:
+        ; volume = pi * radius^2 * height
+
+        LDA     #3      ; Load pi value into accumulator
+        STA     $01FF   ; Store it in memory location $01FF
+
+        LDA     RADIUS
+        STA     $00FF   ; Store radius value in memory location $00FF
+
+        LDA     HEIGHT
+        STA     $0100   ; Store height value in memory location $0100
+
+        LDX     $00FF   ; Load radius value from memory into X register
+        LDY     $0100   ; Load height value from memory into Y register
+
+        ; Calculate radius squared
+        JSR     CALC_SQUARE
+
+        ; Multiply by pi
+        JSR     MULTIPLY
+
+        ; Multiply by height
+        JSR     MULTIPLY
+
+        ; Store result in accumulator
+        STA     VOLUME
+
+        RTS
+
+CALC_SQUARE
+        STX     $00FE   ; Store X value in memory location $00FE
+
+        LDX     $00FE   ; Load X value from memory into X register
+        JSR     MULTIPLY ; Calculate X * X (X^2)
+
+        RTS
+
+MULTIPLY
+        LDA     $00FF   ; Load low byte of first operand into accumulator
+        STA     $00FC   ; Store it in memory location $00FC
+
+        LDA     $0100   ; Load high byte of first operand into accumulator
+        STA     $00FD   ; Store it in memory location $00FD
+
+        LDA     $01FF   ; Load low byte of second operand into accumulator
+        STA     $00FB   ; Store it in memory location $00FB
+
+        LDA     VOLUME  ; Initialize volume to 0
+        STA     $00FA   ; Store it in memory location $00FA
+
+        LDX     #0      ; Initialize carry flag to 0
+        LDY     #0
+
+LOOP    ADC     $00FB   ; Add second operand to accumulator
+        BCC     SKIP    ; Skip if no carry
+        INY             ; Increment high byte
+        TAY
+SKIP    ADC     $00FC   ; Add first operand to accumulator
+        BCC     SKIP2   ; Skip if no carry
+        INX             ; Increment low byte
+        TXA
+SKIP2   ADC     $00FA   ; Add to result (volume)
+        STA     $00FA   ; Store result in memory location $00FA
+        LDX     $00FF   ; Load low byte of first operand
+        STX     $00FC   ; Store it in memory location for next iteration
+        LDX     $0100   ; Load high byte of first operand
+        STX     $00FD   ; Store it in memory location for next iteration
+        LDA     #0      ; Clear accumulator
+        ADC     #0      ; Clear accumulator
+        BCC     LOOP    ; Continue loop until multiplication is complete
+
+        RTS
+
+RADIUS  .byte   5      ; Radius value
+HEIGHT  .byte   10     ; Height value
+VOLUME  .byte   0      ; Volume value
+
+        .end
