dea669ae49b64ea586ca91fb981522407fabc305
Austin L. Howard <austin.lane.howard@gmail.com>
2025-04-22 15:29:50 -0600

    Updated UI components for a more modern look

--- script_version.asm
+++ script_version.asm
@@ -1,29 +1,61 @@
 
-; 6502 Assembly program to check if a given string is an isogram
+        .org $0200
 
-        .org $0200      ; Start program at memory address $0200
+number  .eq $FD
+result  .eq $FE
+index   .eq $FF
 
-        LDX #0         ; Initialize index to zero
-loop    LDA str,X      ; Load character at index X from string
-        BEQ is_isogram ; If end of string is reached, it's an isogram
-check   CMP str,X      ; Compare character at index X with current character
-        BEQ not_isogram; If characters are equal, it's not an isogram
-        INX            ; Increment index
-        CMP #$00       ; Check if end of string is reached
-        BEQ is_isogram ; If end of string is reached, it's an isogram
-        JMP loop       ; Continue looping
-is_isogram
-        LDA #$01       ; Load 1 to indicate isogram
-        STA result     ; Store result
-        BRK            ; End program
+start   lda #0             ; Clear all registers
+        sta number
+        sta result
+        sta index
 
-not_isogram
-        LDA #$00       ; Load 0 to indicate not isogram
-        STA result     ; Store result
-        BRK            ; End program
+        ldx #2             ; Initialize loop index to 2
 
-        .org $0300      ; Data section
-str     .byte "hello",0 ; String to check for isogram
-result  .byte 0         ; Result of isogram check
+loop    lda number         ; Load number into accumulator
+        jsr is_carmichael  ; Call subroutine to check if number is a Lucas-Carmichael number
+        bne not_carmichael ; Branch if not Lucas-Carmichael
+        jmp done           ; Jump to done if number is Lucas-Carmichael
 
-        .end           ; End of program
+not_carmichael
+        inx                ; Increment index
+        cpx number         ; Compare index to input number
+        bcc loop           ; Branch if not done checking all possible divisors
+
+        ldx #1             ; Set result to 1 (false) if number is not a Lucas-Carmichael number
+        sta result
+
+done    rts
+
+is_carmichael
+        lda number         ; Load number into accumulator
+        cmp index          ; Compare number to index
+        beq is_prime       ; Branch if equal
+
+        div index          ; Divide number by index
+        clc
+        ror                ; Rotate to get the remainder
+        bne not_divisible  ; Branch if remainder is not 0
+
+        lda index
+        clc
+        adc #1             ; Add 1 to index
+        sta index
+        jmp is_carmichael  ; Continue checking if number is divisible by index
+
+is_prime
+        lda index
+        cmp #2             ; Check if index is 2
+        beq dividable
+        rts
+
+dividable
+        lda index
+        cmp #2             ; Check if index is 2
+        beq not_divisible
+        rts
+
+not_divisible
+        ldx #1             ; Set result to 1 (false) if number is not a Lucas-Carmichael number
+        sta result
+        rts
