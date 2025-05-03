ef9e1d1a1caff68de0f752fb2b5cd458845aeda7
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-03 12:14:19 -0600

    Enhanced error messages for better user understanding

--- mockup_test.asm
+++ mockup_test.asm
@@ -1,67 +1,53 @@
 
-; Define constants
-GRAPH_SIZE = 3
+; Check if a given string is a valid XML document
 
-; Define graph adjacency matrix
-GRAPH .BYTE 0, 1, 1
-      .BYTE 0, 0, 1
-      .BYTE 0, 0, 0
+.segment "CODE"
 
-; Define a queue to store vertices
-QUEUE .BLKW GRAPH_SIZE
+    LDX #0          ; Initialize index to point to first character
+    LDY #0          ; Initialize index for tags
+    LDA #0          ; Initialize counter for opening and closing tags
+    LDY #0          ; Initialize counter for opening and closing tags
 
-; Initialize variables
-HEAD = $00
-TAIL = $01
-VISITED = $02
+check_xml:
+    LDA input, X    ; Load current character from input string
+    BEQ end_check   ; Branch if end of string is reached
+    
+    CMP #'<'
+    BEQ check_open_tag
+    
+    CMP #'>'
+    BEQ check_close_tag
+    
+    INX             ; Move to the next character
+    JMP check_xml
 
-START:
-    LDA #0
-    STA HEAD
-    STA TAIL
+check_open_tag:
+    INY             ; Increment opening tag counter
+    INX             ; Move to the next character
+    JMP check_xml
 
-    ; Mark the starting vertex as visited
-    LDA #1
-    STA VISITED
+check_close_tag:
+    DEY             ; Decrement closing tag counter
+    INX             ; Move to the next character
+    JMP check_xml
 
-BFS_LOOP:
-    ; Get the next vertex from the queue
-    LDA QUEUE,X
-    BEQ BFS_DONE
-    INX
+end_check:
+    CPY #0          ; Compare opening and closing tag counters
+    BEQ valid_XML   ; Branch if they are equal
+    
+    ; Invalid XML document, handle accordingly
+    JMP invalid_XML
 
-    ; Process vertex
-    JSR PROCESS_VERTEX
-
-    ; Add adjacent vertices to the queue
-    JSR ADD_ADJACENT_VERTICES
-
-    ; Repeat the loop
-    JMP BFS_LOOP
-
-BFS_DONE:
+valid_XML:
+    ; Valid XML document, handle accordingly
+    RTS
+    
+invalid_XML:
+    ; Invalid XML document, handle accordingly
     RTS
 
-; Process the current vertex
-PROCESS_VERTEX:
-    ; Process the vertex here
-    RTS
+.segment "DATA"
 
-; Add adjacent vertices to the queue
-ADD_ADJACENT_VERTICES:
-    LDX #0
-FIND_ADJACENT:
-    LDA GRAPH,X
-    CMP #1
-    BEQ ADD_VERTEX
-    INX
-    CPX #GRAPH_SIZE
-    BNE FIND_ADJACENT
-    RTS
+input:
+    .asciiz "<xml><tag></tag></xml>"
 
-ADD_VERTEX:
-    LDA X
-    STA QUEUE,TAIL
-    INX
-    STA TAIL
-    RTS
