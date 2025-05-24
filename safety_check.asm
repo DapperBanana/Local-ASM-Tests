cb297f3516306511b07214a0fe59032bd61710e7
Austin L. Howard <austin.lane.howard@gmail.com>
2025-05-24 17:07:35 -0600

    Fixed broken unit tests in module HH

--- safety_check.asm
+++
@@ -1,8 +0,0 @@
-
-        LDA #$00           ; initialize accumulator
-LOOP    LDX data          ; load data
-        ; perform operations on data
-        ; store result back in memory
-        INX                ; increment X
-        CPX #end_of_data   ; compare X with end of data address
-        BNE LOOP           ; loop until end of data
