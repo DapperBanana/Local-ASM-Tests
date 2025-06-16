a6428d353ffaa3ad69421d67c279e3bbf515cbf3
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-16 13:51:19 -0600

    Implemented data caching for faster retrieval

--- sample_code.asm
+++ sample_code.asm
@@ -1,51 +1,49 @@
 
-; File Compression Algorithm
-;
-; This program reads a file from memory, compresses it using a basic algorithm,
-; and stores the compressed data back into memory.
+; Program to count the number of occurrences of each character in a string
 
-ORG $1000
+; Input: Address of the string in memory (pointer in zero page)
+; Output: Counts of each character stored in memory starting at 0x0300
 
-; Define constants
-SOURCE_ADDR = $2000
-DEST_ADDR = $3000
-BUFFER_SIZE = 256
+        .org $0200      ; Start of program
+start:
+        LDX #0          ; Initialize index
+        STX COUNT       ; Clear count memory location
+loop:
+        LDA (STRING),X  ; Load character from string
+        BEQ end         ; Exit loop if end of string
+        JSR increment   ; Increment count for character
+        
+        INX             ; Move to next character
+        BNE loop        ; Loop until end of string is reached
+        
+end:
+        RTS             ; Return from subroutine
+        
+increment:
+        STA CHAR        ; Store character to check
+        LDX #0          ; Initialize index for counting
+check:
+        LDA CHAR        ; Load character to check
+        CMP (COUNT),X   ; Check if character matches current count
+        BEQ found       ; Continue if match found
+        INX             ; Move to next count
+        BEQ notFound    ; Jump if end of count memory reached
+        BNE check       ; Continue checking
+        
+found:
+        INC (COUNT),X   ; Increment count for character
+        RTS             ; Return from subroutine
+        
+notFound:
+        STA (COUNT),X   ; Store new character in count memory
+        INC (COUNT),X   ; Increment count for new character
+        RTS             ; Return from subroutine
 
-START:
-    LDA #BUFFER_SIZE
-    STA BUFFER_SIZE       ; Initialize buffer size
-    
-    LDX #0
-    LDY #0
-    
-READ_LOOP:
-    LDA SOURCE_ADDR, X    ; Read a byte from source
-    BEQ COMPRESS          ; If end of file is reached, go to compression
-    STA DEST_ADDR, Y      ; Store byte in destination
-    INX
-    INY
-    DEC BUFFER_SIZE
-    BNE READ_LOOP         ; Continue reading until buffer is full
-    
-COMPRESS:
-    LDX #0
-    LDY #0
-    LDA #BUFFER_SIZE
-    STA BUFFER_SIZE
-    
-COMPRESS_LOOP:
-    LDA DEST_ADDR, X      ; Read a byte from destination
-    CMP $00,X             ; Compare with next byte
-    BEQ COMPRESSED        ; If they are the same, go to compressed
-    STA DEST_ADDR, Y      ; Store byte in destination
-    INX
-    INY
-    DEC BUFFER_SIZE
-    BNE COMPRESS_LOOP     ; Continue compression until buffer is empty
-    
-COMPRESSED:
-    ; End of compression algorithm, compressed file stored in memory
-    
-    RTS
+        .org $0300      ; Start of count memory
+COUNT:  .ds 256         ; Reserve space for character counts
+        
+        .org $0400      ; Start of character string
+STRING:                    ; Input string (replace with desired string)
+        .byte "hello, world!", 0
 
-; Add your file loading code here
+        .end
