3f8f62858fb8a15c6aa3309a0f1744696ca43222
Austin L. Howard <austin.lane.howard@gmail.com>
2025-06-24 17:12:47 -0600

    Updated project logo and branding

--- safety_version.asm
+++ safety_version.asm
@@ -1,50 +1,36 @@
 
-RESET:
-    LDA #$00     ; Initialize memory locations
-    STA NAME
-    STA AGE
-    STA POSITION
+.start:
+    LDA #0         ; Load accumulator with 0
+    STA posx        ; Store starting x position
+    STA posy        ; Store starting y position
 
-    LDX #$00     ; Initialize index register
+.mazeLoop:
+    JSR printMaze   ; Print current maze state
+    JSR solveMaze   ; Solve the maze
 
-WELCOME:
-    LDA MESSAGE,X   ; Load welcome message
-    JSR PRINT       ; Display message
-    INX             ; Increment index register
-    CPX #MESSAGE_LENGTH
-    BNE WELCOME
+    LDA posx       ; Load current x position
+    CMP #5          ; Compare to the maze exit x position
+    BEQ .mazeDone   ; If x position is at the exit, we are done
+    JMP .mazeLoop   ; Otherwise, continue solving the maze
 
-GET_NAME:
-    LDA NAME_PROMPT,X   ; Load name prompt message
-    JSR PRINT           ; Display message
-    JSR GET_INPUT       ; Get user input
-    STA NAME            ; Store user input in memory
-    ...
+.mazeDone:
+    BRK             ; Exit the program
 
-PRINT:
-    ; Output message to screen
-    RTS
-    
-GET_INPUT:
-    ; Get user input and store in memory
+printMaze:
+    ; Implement maze printing routine
     RTS
 
-; Define messages and constants
-MESSAGE:
-    .BYTE "Welcome to the job application system!", $00
-    .BYTE "Please enter your name: ", $00
-    ...
+solveMaze:
+    ; Implement maze solving algorithm
+    RTS
 
-NAME_PROMPT:
-    .BYTE "Enter your name: ", $00
+posx:   .BYTE 0
+posy:   .BYTE 0
 
-NAME:
-    .BYTE $00
-
-AGE:
-    .BYTE $00
-
-POSITION:
-    .BYTE $00
-
-MESSAGE_LENGTH = * - MESSAGE
+mazeData:
+    .BYTE "######"
+    .BYTE "#    #"
+    .BYTE "# ## #"
+    .BYTE "# ## #"
+    .BYTE "#    #"
+    .BYTE "######"
