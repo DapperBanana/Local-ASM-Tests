b19241a60b3129c9f72a6f257d29e6c3181c3623
Austin L. Howard <austin.lane.howard@gmail.com>
2025-08-25 13:35:47 -0600

    Resolved compatibility issues with library/library version Z

--- initial_test.asm
+++ initial_test.asm
@@ -1,42 +1,22 @@
 
-ORG $1000
-
-START:
-    LDX #$00         ; Initialize index for first string
-    LDY #$00         ; Initialize index for second string
-    
+LDA #0              ; Initialize index pointer to 0
+    STA $00
 LOOP:
-    LDA STRING1, X  ; Load character from first string
-    CMP #0           ; Check if end of string reached
-    BEQ END
-    
-    LDA STRING2, Y  ; Load character from second string
-    CMP #0           ; Check if end of string reached
-    BEQ END
-    
-    CMP STRING1, Y  ; Compare character from first string with character from second string
-    BEQ MATCH       ; If characters match, branch to MATCH
-    
-    INX             ; Increment index for first string
-    INY             ; Increment index for second string
-    BRA LOOP
-    
-MATCH:
-    STA RESULT, X   ; Store the common character in result string
-    INX             ; Increment index for result string
-    INY             ; Increment index for second string
-    BRA LOOP
+    LDA INPUT,X     ; Load character from input string
+    CMP #65         ; Compare with ASCII value for 'a'
+    BCC NOT_LOWERCASE    ; If character is not lowercase, skip to next character
+    CMP #122        ; Compare with ASCII value for 'z'
+    BCS NOT_LOWERCASE    ; If character is not lowercase, skip to next character
+    SBC #32         ; Convert lowercase character to uppercase
+    STA OUTPUT,X    ; Store converted character in output string
+NOT_LOWERCASE:
+    INX             ; Increment index pointer
+    CPX #STRING_SIZE    ; Compare with the string size
+    BNE LOOP        ; If index is not equal to the string size, continue loop
+    RTS             ; Return from subroutine
 
-END:
-    LDA #0           ; Null terminate the result string
-    STA RESULT, X
-    RTS
-
-STRING1:
-    .BYTE "HELLO", 0   ; First string
-STRING2:
-    .BYTE "WORLD", 0   ; Second string
-RESULT:
-    .BYTE $00          ; Result string
-
-    END START
+INPUT:
+    .BYTE "hello, world!",0
+OUTPUT:
+    .BYTE 20 DUP (?)
+STRING_SIZE = * - INPUT - 1
