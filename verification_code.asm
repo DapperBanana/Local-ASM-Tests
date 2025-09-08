1f18ce264b920fbb38137156dfbb456dfd484526
Austin L. Howard <austin.lane.howard@gmail.com>
2025-09-08 11:30:06 -0600

    Added support for user roles in the application

--- verification_code.asm
+++ verification_code.asm
@@ -1,20 +1,92 @@
 
-; Program to calculate the square of each element in a list using 6502 assembly language.
+ORG $0200
+start:
+    LDX #0         ; Initialize index to 0
+    LDA numbers, X ; Load the first number from the list
+    STA result     ; Initialize result with the first number
+    
+loop:
+    INX            ; Increment index
+    CPX #$0B       ; Check if end of list has been reached
+    BEQ done       ; If end of list, go to done
+    
+    LDA numbers, X ; Load next number from list
+    JSR calc_lcm   ; Calculate LCM of current result and new number
+    BRA loop       ; Repeat the loop
+    
+done:
+    BRK
 
-    .org $0200        ; Start address of program
-    .memory $0200,$02FF    ; Define memory range for variables
-    ; Define variables
-list    .byte $02, $04, $06, $08, $0A             ; List of numbers
-result  .byte $00, $00, $00, $00, $00            ; List to store square of each number
+calc_lcm: 
+    PHA            ; Save the accumulator on stack
+    PHX            ; Save the index on stack
+    
+    ; Calculate the LCM of two numbers
+    LDX result     ; Load result value
+    CLC
+    ADC numbers, X ; Add the next number
+    STA acc        ; Store the result in acc
+    LDA result     ; Load result value
+    JSR gcd        ; Call the subroutine to calculate GCD
+    LDX acc        ; Load GCD value in X 
+    LDA acc        ; Load GCD value in A
+    SEC
+    SBC acc        ; Sub the GCD value from Accumulator 
+    TAY            ; Load result to Y Register
+    LDA numbers, X ; Load number from list in A
+    JSR mul        ; Multiply the number with calculated GCD
+    LDX acc        ; Load the result in X
+    STX result     ; Store the result back in result
+    
+    PLX            ; Restore the index
+    PLA            ; Restore the accumulator
+    RTS
 
-    LDX #0              ; Initialize index register X to 0
-loop    LDA list, X        ; Load element from list
-    STA $00           ; Store the element in memory location $00
-    ASL $00           ; Multiply the element by itself (square)
-    STA result, X      ; Store the result in result list
-    INX               ; Increment index register
-    CPX #5            ; Check if end of list reached
-    BNE loop          ; If not, repeat loop
-    BRK               ; End of program
+gcd:
+    PHA  ; Save the accumulator on stack
+    PHX  ; Save the index on stack
 
-    .end
+gcd_loop:
+    CPX #$0B ; Check if index has reached end of list
+    BEQ gcd_done ; If yes, return
+    
+    LDA result
+    SEC
+    SBC numbers, X ; Subtract each number in the list from result
+    STA result
+    BEQ gcd_done ; If result becomes 0, return
+    
+    INX
+    BRA gcd_loop
+
+gcd_done:
+    PLA  ; Restore the index
+    PLX  ; Restore the accumulator
+    RTS
+
+mul:
+    PHA  ; Save the accumulator on stack
+    PHX  ; Save the index on stack
+
+mul_loop:
+    CPX acc  ; Check if index has reached number of times the GCD
+    BEQ mul_done ; If yes, return
+    
+    CLC
+    ADC acc
+    DEX  ; decrement the counter
+    BRA mul_loop
+
+mul_done:
+    PLA  ; Restore the index
+    PLX  ; Restore the accumulator
+    RTS
+
+numbers:
+    .BYTE $0A, $0B, $0F, $12, $15 ; List of numbers
+result:
+    .BYTE $00 ; Store LCM result
+acc:
+    .BYTE $00 ; Store temporary result
+
+    JMP start
