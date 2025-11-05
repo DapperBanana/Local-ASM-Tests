bd8cd3047b2b908b035270ae8eae821b7c107293
Austin L. Howard <austin.lane.howard@gmail.com>
2025-11-04 17:52:07 -0700

    Fixed accessibility issues in the UI

--- reliability_check_code.asm
+++ reliability_check_code.asm
@@ -1,63 +1,24 @@
 
-ORG $1000 
+; Initialize program
 
-START:
-    LDX #0         ; Initialize index register X to 0
-    LDA JOB_PROMPT ; Load job application prompt
-LOOP:
-    JSR PRINTSTR   ; Print the prompt
-    JSR READLINE   ; Read user input
-    CMP #0         ; Check if input is empty
-    BEQ ENDLOOP    ; Exit loop if empty
-    JSR SAVEINPUT  ; Save user input
-    INX            ; Increment index
-    BNE LOOP       ; Loop back to prompt
-ENDLOOP:
-    RTS            ; Return from subroutine
+LDA #$00 ; Clear accumulator
+STA $6000 ; Set start address for image memory
 
-JOB_PROMPT:
-    .BYTE 13,10,"Enter your name: ",0
+LDA #$00 ; Set X coordinate to 0
+LDX #$00 ; Set Y coordinate to 0
 
-SAVEINPUT:
-    STX INPUT_INDEX ; Save current index to memory
-    STX INPUT_LENGTH ; Save current input length to memory
-    LDA #0          ; Reset index
-    STA INPUT_INDEX
-    RTS
+loop:
+; Generate random pixel color
+LDA #*$05 ; Load color value (0-255) into accumulator
+STA $6000,X ; Store color value at current memory address
 
-PRINTSTR:
-    LDX #0          ; Initialize index register X to 0
-NEXTCHAR:
-    LDA (MESSAGE),X ; Load next character from message
-    BEQ PRINTDONE   ; If end of message, print done
-    JSR PRINTCHAR   ; Print character
-    INX             ; Increment index
-    BNE NEXTCHAR    ; Continue to next character
-PRINTDONE:
-    RTS
+INX ; Increment X coordinate
+CPX #$80 ; Check if reached end of row
+BNE loop ; If not, continue to next pixel
+LDX #$00 ; Reset X coordinate to 0
 
-PRINTCHAR:
-    JSR $FFD2      ; ROM routine to print character
-    RTS
+INY ; Increment Y coordinate
+CPY #$50 ; Check if reached end of image
+BNE loop ; If not, continue to next row
 
-READLINE:
-    LDX #0          ; Initialize index register X to 0
-NEXTKEY:
-    JSR $FFCF      ; ROM routine to get keypress
-    CMP #13        ; Check for enter key
-    BEQ ENDLINE    ; End input if enter key
-    JSR PRINTCHAR  ; Print character
-    INX            ; Increment index
-    BNE NEXTKEY    ; Continue to next character
-ENDLINE:
-    LDA #0         ; Null-terminate input
-    JSR PRINTCHAR
-    RTS
-
-INPUT_INDEX: .BYTE 0
-INPUT_LENGTH: .BYTE 0
-
-MESSAGE:
-    .BYTE 13,10,"Thank you for applying!",13,10,0
-
-EOF:
+RTS ; End program
